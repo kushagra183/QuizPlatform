4638c7d0aaf95594ce56d1ca8dd061395d5b36a4
Sagarika Kar <sagarikakar365@gmail.com>
2022-12-06 20:00:57 +0530

    added homepage & Shifted navbar from index.js to app.js

--- metagetterapp/src/App.js
+++ metagetterapp/src/App.js
@@ -1,9 +1,10 @@
+import Navbar from './Components/Navbar/Navbar';
+import HomePage from './Components/Pages/HomePage';
 function App() {
   return (
-    <div className="App" style={{marginTop:"100px"}}>
-      Sample test/first commit
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci vel molestias nostrum possimus ullam, maiores, animi doloremque illo culpa nisi impedit recusandae delectus? Ex, cumque? Rem repudiandae, maiores error beatae tenetur quam earum quasi dolore molestiae eaque. Perspiciatis sequi tempore deleniti alias dignissimos, dolore at? Maxime mollitia vitae enim minus.
-  
+    <div  >
+      <Navbar />
+      <HomePage />
     </div>
   );
 }
--- metagetterapp/src/index.js
+++ metagetterapp/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Navbar from './Components/Navbar/Navbar';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navbar/>
+    
     <App />
   </React.StrictMode>
 );
