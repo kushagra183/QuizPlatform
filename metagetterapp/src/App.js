41a7fae0c971c628ea5665cfc7e89302dcb6c736
Sagarika Kar <sagarikakar365@gmail.com>
2022-12-06 20:41:49 +0530

    Footer added

--- metagetterapp/src/App.js
+++ metagetterapp/src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './Components/Navbar/Navbar';
 import HomePage from './Components/Pages/HomePage';
+import Footer from './Components/Footer/Footer';
 function App() {
   return (
     <div  >
       <Navbar />
       <HomePage />
+      <Footer />
     </div>
   );
 }
